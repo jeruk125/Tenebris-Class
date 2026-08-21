81e3cabe9e6fbff7dbce3c72445ca96dd13096d1
Aditia Muharam Hidayat <aditiamuharamh@gmail.com>
2026-08-21 16:51:33 +0700

    gitignore

--- app.py
+++ app.py
@@ -651,4 +651,8 @@ def create_admin():
 
 if __name__ == '__main__':
     create_admin()
-    app.run(debug=os.environ.get('FLASK_DEBUG', 'False').lower() == 'true')
+    app.run(
+        host=os.environ.get('FLASK_HOST', '0.0.0.0'),
+        port=int(os.environ.get('FLASK_PORT', 5000)),
+        debug=os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
+    )
